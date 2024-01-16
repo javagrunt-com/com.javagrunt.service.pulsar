9bdf7ce0a9c0c1916511bc202d25645fedce1313
dashaun <dashaun@dashaun.com>
2024-01-16 08:41:56 -0600

    initializr, initializr-plusplus

---
+++ src/test/java/com/javagrunt/service/pulsar/TestPulsarApplication.java
@@ -0,0 +1,30 @@
+package com.javagrunt.service.pulsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PulsarContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestPulsarApplication {
+
+	@Bean
+	@ServiceConnection
+	PulsarContainer pulsarContainer() {
+		return new PulsarContainer(DockerImageName.parse("apachepulsar/pulsar:latest"));
+	}
+
+	@Bean
+	@ServiceConnection(name = "openzipkin/zipkin")
+	GenericContainer<?> zipkinContainer() {
+		return new GenericContainer<>(DockerImageName.parse("openzipkin/zipkin:latest")).withExposedPorts(9411);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(PulsarApplication::main).with(TestPulsarApplication.class).run(args);
+	}
+
+}
